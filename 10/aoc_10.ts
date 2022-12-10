9361e6630b713fba482f06a54d5433a0f7b9e242
RayWiis <rejus.paukst@gmail.com>
2022-12-10 11:09:51 +0200

    Day 10 success

---
+++ 10/aoc_10.ts
@@ -0,0 +1,40 @@
+// const input = (await Deno.readTextFile('./sample')).split('\n');
+const input = (await Deno.readTextFile('./input')).split('\n');
+
+function runVM(instructions: string[]) {
+  const signalStr: number[] = []
+  const crt: string[][] = Array.from({length: 8}).map(() => ([]))
+  let cycle = 0;
+  let x = 1;
+
+  const moveCycle =(count: number) =>{
+    const start = cycle;
+    while (cycle < start + count) {
+      cycle += 1;
+      if ((cycle - 20) % 40 === 0) {
+        signalStr.push(x * cycle);
+      }
+      const row = Math.floor(cycle / 40)
+      const pixel = cycle % 40 - 1;
+      const inSignal = pixel <= x + 1 && pixel >= x- 1;
+      crt[row][pixel] = inSignal ? '#' : '.';
+    }
+  }
+
+  for (const instruction of instructions) {
+    if (instruction === 'noop') {
+      moveCycle(1);
+    } else if (instruction.startsWith('addx')){
+      const n = parseInt(instruction.split(' ')[1], 10)
+      moveCycle(2);
+      x += n;
+    } else {
+      throw new Error('unknown instruction'+ instruction)
+    }
+  }
+  return [signalStr, crt] as const
+}
+
+const [out,crt] = runVM(input)
+console.log(out.reduce((a, b) => a + b))
+console.log(crt.map(r => r.join('')).join('\n'))
